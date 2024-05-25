2fe342dc1dac275be574f3400dc20974b6e31407
Shokhrukh <shoxrux20888@mail.ru>
2024-05-25 20:51:26 +0300

    fix/modified: header/main/footer

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,12 @@ function createItemForList() {
     let paragraph = document.createElement('p');
     let xmlSvg = document.createElement('div');
     let button = document.createElement('button');
+    const currentDate = new Date().toLocaleString('ru',{
+    month: 'numeric',
+    day: 'numeric'});
     li.className = 'mediaPlan__item';
     paragraph.className = 'mediaPlan__item-text';
-    paragraph.innerHTML = 'Companyname 10/23';
+    paragraph.innerHTML = `Companyname ${currentDate}`;
     xmlSvg.className = 'mediaPlan__item-xmlLogo';
     xmlSvg.innerHTML = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M8 2H17C19 2 20 3 20 5V6.38" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
@@ -30,10 +33,12 @@ function createItemForList() {
 
 
 function sendMessage() {
+    let content = document.querySelector('#main__content__office__chat-inner');
     let message = document.getElementById("message").value;
     if (message.trim() !== "") {
         document.getElementById("chat-box").innerHTML += `<p>${message}</p>`;
         document.getElementById("message").value = "";
+        content.style.overflow = 'scroll';
     }
 }
 
@@ -46,6 +51,9 @@ function requestMediaPlan() {
     let media = document.querySelector('#media__inner');
     text.style.display = 'none';
     logo.style.display = 'none';
+    const currentDate = new Date().toLocaleString('ru',{year:'numeric',
+        month: 'numeric',
+        day: 'numeric'});
     document.querySelector('#media').style.display = 'block';
     let mediaList = document.querySelector('#media__inner-list');
     let specText = document.createElement('div');
@@ -70,7 +78,7 @@ function requestMediaPlan() {
             <path d="M16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8Z" fill="#1CC437"/>
             <path d="M3.83333 7.375L5.64226 9.1387C6.41901 9.89604 7.65791 9.89604 8.43466 9.1387L12.1667 5.5" stroke="white" stroke-width="1.5"/>
             </svg>
-            Медиаплан от 1.06.22 готов`;
+            Медиаплан от ${currentDate} готов`;
             setTimeout(() => {
             specText.style.display = 'none';
 
@@ -91,13 +99,16 @@ function requestReport() {
     let report = document.querySelector('#report__inner');
     text.style.display = 'none';
     logo.style.display = 'none';
+    const currentDate = new Date().toLocaleString('ru',{year:'numeric',
+        month: 'numeric',
+        day: 'numeric'});
     document.querySelector('#report').style.display = 'block';
     let reportList = document.querySelector('#report__inner-list');
     let specText = document.createElement('div');
     specText.innerHTML = `<span><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM8.26136 10.2727L8.34659 4H7.1875L7.27273 10.2727H8.26136ZM7.22585 12.5696C7.37642 12.7202 7.55682 12.7955 7.76705 12.7955C7.90909 12.7955 8.03693 12.7614 8.15057 12.6932C8.26705 12.6222 8.35938 12.5284 8.42756 12.4119C8.49858 12.2955 8.53409 12.1676 8.53409 12.0284C8.53409 11.8182 8.45881 11.6378 8.30824 11.4872C8.15767 11.3366 7.97727 11.2614 7.76705 11.2614C7.55682 11.2614 7.37642 11.3366 7.22585 11.4872C7.07528 11.6378 7 11.8182 7 12.0284C7 12.2386 7.07528 12.419 7.22585 12.5696Z" fill="#949494"/>
     </svg>
-    </span>Медиаплан в процессе составления`;
+    </span>Отчет формируется`;
     specText.className = 'spec__text';
     reportList.appendChild(createItemForList());
     report.appendChild(specText);
@@ -115,7 +126,7 @@ function requestReport() {
             <path d="M16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8Z" fill="#1CC437"/>
             <path d="M3.83333 7.375L5.64226 9.1387C6.41901 9.89604 7.65791 9.89604 8.43466 9.1387L12.1667 5.5" stroke="white" stroke-width="1.5"/>
             </svg>
-            Медиаплан от 1.06.22 готов`;
+            Отчет от ${currentDate} готов`;
             setTimeout(() => {
             specText.style.display = 'none';
 
@@ -173,5 +184,14 @@ function closeMoreReport () {
         report.style.display = 'block';
     } else {
         report.style.display = 'none';
+    }
+}
+
+function openHelperSettings() {
+    let helper = document.querySelector('#main__content__helper');
+    if(helper.style.visibility === 'hidden') {
+        helper.style.visibility = 'visible';
+    } else {
+        helper.style.visibility = 'hidden';
     }
 }
